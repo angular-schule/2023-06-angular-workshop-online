d0ce92e966c2021a6afa726d8e205c27a064e6ac
Ferdinand Malcher <ferdinand@malcher.media>
2023-06-14 11:47:21 +0200

    DashboardComponent

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,22 +4,3 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
-
-///////////////////////////
-
-interface Customer {
-  id: number;
-  name: string;
-  address: string;
-}
-
-
-function foo(arg: Customer) {}
-
-
-const myCustomer: Customer = {
-  id: 5,
-  name: '',
-  address: ''
-};
