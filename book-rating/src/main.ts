c2fb888b2c0e41dd2268262f02107642c0947a1d
Ferdinand Malcher <ferdinand@malcher.media>
2023-06-14 10:38:43 +0200

    aufräumen

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,39 +4,3 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
-////////////////////////////
-
-export const foo = 5;
-export function bar() {}
-
-export class Customer {
-
-  static foo = 5;
-
-  /*private id: number;
-
-  constructor(idx: number) {
-    this.id = idx;
-  }*/
-
-
-  constructor(public id: number) {}
-
-
-  fooBar(arg: string): number {
-    setTimeout(() => {
-      console.log('Hallo', this.id);
-    }, 2000);
-    return 5;
-  }
-}
-
-const myCustomer = new Customer(4);
-const result = myCustomer.fooBar('');
-console.log(result);
-
-
-
-
-
