e230b3b4799c3bec0e6f7ec37a439abda36eed89
Ferdinand Malcher <ferdinand@malcher.media>
2023-06-16 11:35:27 +0200

    Test Dashboard

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,21 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(() => {
+    const ratingMock = {
+      rateUp: (b: Book) => b
+    };
+
     TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [DashboardComponent],
+      providers: [
+        // BRS ersetzen: Immer, wenn jemand BRS anfordert,
+        // wird stattdessen ratingMock ausgeliefert
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     });
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+
+    // Zugriff auf das DOM-Element
+    // fixture.nativeElement.querySelector('p').textContent
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call service.rateUp for component.doRateUp()', () => {
+    // Arrange
+    // BRS anfordern, tatsächlich ist das aber unser ratingMock
+    const service = TestBed.inject(BookRatingService);
+
+    // Testbuch
+    const testBook = { isbn: '123' } as Book; // Type Assertion
+
+    // ratingMock überwachen (spyOn ersetzt die Methode)
+    // spyOn(service, 'rateUp').and.callFake(b => b);
+    // spyOn(service, 'rateUp').and.returnValue(testBook);
+    spyOn(service, 'rateUp').and.callThrough();
+
+    // Act
+    component.doRateUp(testBook);
+
+    // Assert
+    expect(service.rateUp).toHaveBeenCalled()
+    expect(service.rateUp).toHaveBeenCalledTimes(1);
+    expect(service.rateUp).toHaveBeenCalledWith(testBook);
+    expect(service.rateUp).toHaveBeenCalledOnceWith(testBook);
   });
 });
