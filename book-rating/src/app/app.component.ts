a617e36fb06e1de2a3dc9df44bb6b71feb44f74f
Ferdinand Malcher <ferdinand@malcher.media>
2023-06-19 13:40:28 +0200

    Vorbereitung Formmulare und Suche

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive]
 })
 export class AppComponent {
   title = 'book-rating';
