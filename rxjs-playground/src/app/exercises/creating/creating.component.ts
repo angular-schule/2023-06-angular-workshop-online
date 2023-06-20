a65b01192cf5858ab161e5076b96b6ae495b5467
Ferdinand Malcher <ferdinand@malcher.media>
2023-06-20 13:29:17 +0200

    Fun with Observables

--- rxjs-playground/src/app/exercises/creating/creating.component.ts
+++ rxjs-playground/src/app/exercises/creating/creating.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter, Observer, Subscriber, take } from 'rxjs';
 import { HistoryComponent } from '../../shared/history/history.component';
 
 @Component({
@@ -24,7 +24,56 @@ export class CreatingComponent {
 
     /******************************/
 
-    
+    // of('A', 'B', 'C')
+    // from([1,2,3,4,5,6])
+    // interval(1000)        // ---0---1---2---3---4--- ...
+    // timer(2000)           // ------0|
+    // timer(4000, 1000)     // ------------0---1---2---3---4--- ...
+
+    timer(0, 1000).pipe(
+      map(e => e * 3),
+      filter(e => e % 2 === 0)
+    ).subscribe({
+      next: e => this.log(e),
+      error: e => console.log(e),
+      complete: () => this.log('COMPLETE')
+    });
+
+
+    /******************************/
+
+    function producer(sub: Subscriber<number>) {
+      const result = Math.random();
+      sub.next(result);
+
+      const result2 = 1;
+      sub.next(result2);
+
+      const timer1 = setTimeout(() => sub.next(5), 2000);
+      const timer2 = setTimeout(() => sub.complete(), 4000);
+
+      // Teardown Logic // beim unsubscribe()
+      return () => {
+        clearTimeout(timer1)
+        clearTimeout(timer2)
+      };
+
+    }
+
+    const obs: Observer<number> = {
+      next: e => console.log(e),
+      error: (e: any) => console.error(e),
+      complete: () => console.log('COMPLETE')
+    };
+
+    // producer(obs);
+
+    const myObs$ = new Observable(producer);
+    // myObs$.subscribe(obs);
+
+
+
+
     /******************************/
   }
 
